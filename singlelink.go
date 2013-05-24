f03714d184193931ab576772458a71134efc2e8d
Bill DeRose <bill.derose11@gmail.com>
2013-05-23 17:13:49 -0700

    Documentation update

--- singlelink.go
+++ singlelink.go
@@ -5,10 +5,13 @@ type SingleLink struct {
 	next *SingleLink
 }
 
+
+// Return reference to next link in list
 func (l *SingleLink) Next() *SingleLink {
 	return l.next
 }
 
+// Return value held by link
 func (l *SingleLink) Value() interface{} {
 	return l.val
 }
